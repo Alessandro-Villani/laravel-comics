b1cd66441820403b57805e71926f37406b37e14c
Alessandro-Villani <alessvill@libero.it>
2023-02-23 16:49:00 +0100

    added header and jumbotron

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,6 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ env('APP_NAME') }} | Home </title>
     <link rel="icon" href="{{ asset('images/favicon.ico')}}">
+    {{-- FONTAWESOME --}}
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css' integrity='sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==' crossorigin='anonymous'/>
 
     @yield('cdns')
 
@@ -13,7 +15,9 @@
     @vite('resources/js/app.js')
 </head>
 <body>
-    
+    @include('includes.header')
+    @include('includes.jumbotron')
+
     @yield('content')
 
 </body>
